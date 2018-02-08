99e3cd15806d1d377cdb7ba66ec9aac47c12daed
Jake <jakewgoodman@gmail.com>
2018-02-08 07:29:31 -0500

    Added new file PeriodicTable.java

---
+++ src/main/periodictable/PeriodicTable.java
@@ -0,0 +1,32 @@
+package main.periodictable;
+
+public class PeriodicTable {
+    public final static Element hydrogen = Element.HYDROGEN;
+    public final static Element helium = Element.HELIUM;
+    public final static Element lithium = Element.LITHIUM;
+    public final static Element beryllium = Element.BERYLLIUM;
+    public final static Element boron = Element.BORON;
+    public final static Element carbon = Element.CARBON;
+    public final static Element nitrogen = Element.NITROGEN;
+    public final static Element oxygen = Element.OXYGEN;
+    public final static Element fluorine = Element.FLUORINE;
+    public final static Element neon = Element.NEON;
+
+
+    public static void getElement(Element element) {
+        System.out.printf("Element name: %s%n" +
+                "Element symbol: %s%n" +
+                "Atomic number: %s%n" +
+                "Atomic mass: %s%n" +
+                "Mass number: %s%n" +
+                "Number of neutrons: %s%n",
+                element.getName(),
+                element.getSymbol(),
+                element.getAtomicNum(),
+                element.getAtomicMass(),
+                element.getMassNum(),
+                element.getNumOfNeutrons());
+    }
+
+
+}
