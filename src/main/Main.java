ea1aa22e066271f9d0537008721c89782d3dfc99
Jake <jakewgoodman@gmail.com>
2018-02-08 07:30:50 -0500

    Added new file Main.java

---
+++ src/main/Main.java
@@ -0,0 +1,15 @@
+package main;
+
+
+import main.periodictable.Element;
+import main.periodictable.PeriodicTable;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Element element = Element.FLUORINE;
+
+        PeriodicTable.getElement(PeriodicTable.carbon);
+
+    }
+}
