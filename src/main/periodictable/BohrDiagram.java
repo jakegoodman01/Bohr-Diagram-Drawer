cdcc6e507e4de3b9c53b53ffaabe0db782328200
jakegoodman <jakewgoodman@gmail.com>
2018-02-08 19:13:30 -0500

    Changed the position of the Bohr Diagram on the screen.

--- src/main/periodictable/BohrDiagram.java
+++ src/main/periodictable/BohrDiagram.java
@@ -13,16 +13,19 @@ public class BohrDiagram {
     }
 
     public static void draw (GraphicsContext gc) {
+        gc.clearRect(0, 0, 500, 500);
         gc.setStroke(Color.BLACK);
         gc.setFont(new Font("Helvetica", 50));
 
-        gc.fillText("He", 120, 160);
+        gc.fillText("He", 120, 100);
 
-        int upperBound = 100;
+        int x = 100;
+        int y = 40;
         int width = 100;
         for (int i = 0; i < element.getNumOfOrbits(); i++) {
-            gc.strokeOval(upperBound, upperBound, width, width);
-            upperBound -= 20;
+            gc.strokeOval(x, y, width, width);
+            x -= 20;
+            y -= 20;
             width += 40;
         }
     }
